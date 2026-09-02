8fee0fb3887533966e07ccd80b0849a2a0e3cf68
L. SAI KIRAN <saikiranlakotia@gmail.com>
2026-09-02 23:49:52 +0530

    Time: 0 ms (100%), Space: 30.2 MB (67.8%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,25 @@
+// class Solution {
+// public:
+//     bool uniformArray(vector<int>& nums1) {
+//         return true;
+//         }
+// };
+
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        return true;
+        int odd = 0, even = 0;
+
+        for (int x : nums1) {
+            if (x % 2 == 0)
+                even++;
+            else
+                odd++;
         }
+
+        if (odd == 0 || even == 0)
+            return true;
+
+        return true;
+    }
 };
